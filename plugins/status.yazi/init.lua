6ccd0847e0315980a0aa7c78808d310c612d7aaf
greg <grzegorz.kozub@gmail.com>
2024-06-29 07:27:37 +0200

    windows

--- plugins/status.yazi/init.lua
+++ plugins/status.yazi/init.lua
@@ -20,7 +20,7 @@ function Status:size()
     return ui.Span ''
   end
   local size = h:size() or h.cha.length
-  return ui.Span(' ' .. string.format('%6s', ya.readable_size(size))):fg(size_color(size))
+  return ui.Span(string.format('%6s', ya.readable_size(size))):fg(size_color(size))
 end
 
 local function owner_color(owner)
@@ -39,7 +39,7 @@ function Status:owner()
     return ui.Span ''
   end
   local owner = ya.user_name(h.cha.uid) or tostring(h.cha.uid)
-  return ui.Span(' ' .. owner):fg(owner_color(owner))
+  return ui.Span(owner):fg(owner_color(owner))
 end
 
 local function year(date)
@@ -54,7 +54,7 @@ function Status:modified()
   local modified = math.floor(h.cha.modified)
   local now = math.floor(ya.time())
   local format = year(modified) < year(now) and '%d %b  %Y' or '%d %b %H:%M'
-  return ui.Span(' ' .. tostring(os.date(format, modified)):lower()):fg 'gray'
+  return ui.Span(tostring(os.date(format, modified)):lower()):fg 'gray'
 end
 
 function Status:link()
@@ -63,7 +63,7 @@ function Status:link()
     return ui.Span ''
   end
   local link = '-> ' .. tostring(h.link_to)
-  return ui.Span(' ' .. link):fg(h.cha.is_orphan and 'red' or 'blue')
+  return ui.Span(link):fg(h.cha.is_orphan and 'red' or 'blue')
 end
 
 function Status:mode()
@@ -71,10 +71,7 @@ function Status:mode()
   if mode == 'n' then
     return ui.Span '    '
   end
-  return ui.Line {
-    ui.Span(' ' .. mode .. ' '):style(self.style()),
-    ui.Span ' ',
-  }
+  return ui.Span(' ' .. mode .. ' '):style(self.style())
 end
 
 function Status:position()
@@ -83,17 +80,34 @@ function Status:position()
   return ui.Span(string.format('%2d/%-2d', cursor + 1, length)):fg 'darkgray'
 end
 
+local function space()
+  return ui.Span ' '
+end
+
 function Status:render(area)
   self.area = area
-  local left = ui.Line {
-    self:permissions(),
-    self:size(),
-    self:owner(),
-    self:modified(),
-    self:link(),
-  }
+  local left = ya.target_family() == 'windows'
+      and ui.Line {
+        self:size(),
+        space(),
+        self:modified(),
+        space(),
+        self:link(),
+      }
+    or ui.Line {
+      self:permissions(),
+      space(),
+      self:size(),
+      space(),
+      self:owner(),
+      space(),
+      self:modified(),
+      space(),
+      self:link(),
+    }
   local right = ui.Line {
     self:mode(),
+    space(),
     self:position(),
   }
   return {
