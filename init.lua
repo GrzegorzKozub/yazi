94fdc8e07f1ab1a17449effdb23216f1ee8f37d9
greg <grzegorz.kozub@gmail.com>
2024-09-14 07:58:17 +0200

    ffmpeg & git

--- init.lua
+++ init.lua
@@ -1,3 +1,29 @@
-require('git'):setup()
+-- core
+
 require('session'):setup { sync_yanked = true }
+
+-- git.yazi
+
+THEME.git_ignored = ui.Style():fg 'darkgray'
+THEME.git_ignored_sign = '!'
+
+THEME.git_untracked = ui.Style():fg 'yellow'
+THEME.git_untracked_sign = '*'
+
+THEME.git_added = ui.Style():fg 'green'
+THEME.git_added_sign = '+'
+
+THEME.git_modified = ui.Style():fg 'blue'
+THEME.git_modified_sign = '~'
+
+THEME.git_updated = ui.Style():fg 'blue'
+THEME.git_updated_sign = '~'
+
+THEME.git_deleted = ui.Style():fg 'red'
+THEME.git_deleted_sign = '-'
+
+require('git'):setup()
+
+-- status.yazi
+
 require 'status'
