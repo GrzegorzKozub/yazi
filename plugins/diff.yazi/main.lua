7d7b699ccc4d10a235f936bd6580fe08d8f80062
greg <grzegorz.kozub@gmail.com>
2025-02-15 10:39:25 +0100

    size plugin uses hovered instead of cwd

--- plugins/diff.yazi/main.lua
+++ plugins/diff.yazi/main.lua
@@ -1,39 +1,35 @@
-local function info(content)
-  return ya.notify {
-    title = 'Diff',
-    content = content,
-    timeout = 5,
-  }
-end
+local M = {}
 
-local selected_url = ya.sync(function()
-  for _, u in pairs(cx.active.selected) do
-    return u
-  end
-end)
+-- local get_paths = ya.sync(function()
+--   local paths, selected = {}, nil
+--   for _, value in pairs(cx.active.selected) do
+--     paths[#paths + 1] = tostring(value)
+--   end
+--   if #paths == 0 then
+--     local h = cx.active.current.hovered.url
+--     paths[1] = tostring(h)
+--     selected = h:name()
+--   else
+--     selected = #paths
+--   end
+--   return paths, selected
+-- end)
+--
+-- M.entry = function()
+--   local paths, selected = get_paths()
+--   local total, err = get_total(paths)
+--   if err then
+--     ya.notify { content = err, level = 'error', timeout = 3, title = 'Size' }
+--     return
+--   end
+--   ya.notify {
+--     content = (type(selected) == 'string' and selected .. ': ' or selected .. ' selected: ')
+--       .. total,
+--     -- level = 'info',
+--     timeout = 3,
+--     title = 'Size',
+--   }
+-- end
 
-local hovered_url = ya.sync(function()
-  local h = cx.active.current.hovered
-  return h and h.url
-end)
+return M
 
-return {
-  entry = function()
-    local a, b = selected_url(), hovered_url()
-    if not a then
-      return info 'No file selected'
-    elseif not b then
-      return info 'No file hovered'
-    end
-
-    local output, err = Command('diff'):arg('-Naur'):arg(tostring(a)):arg(tostring(b)):output()
-    if not output then
-      return info('Failed to run diff, error: ' .. err)
-    elseif output.stdout == '' then
-      return info 'No differences found'
-    end
-
-    ya.clipboard(output.stdout)
-    info 'Diff copied to clipboard'
-  end,
-}
--- plugins/size.yazi/main.lua
+++ plugins/size.yazi/main.lua
@@ -1,12 +1,14 @@
 local M = {}
 
 local get_paths = ya.sync(function()
-  local paths, selected = {}, 0
+  local paths, selected = {}, nil
   for _, value in pairs(cx.active.selected) do
     paths[#paths + 1] = tostring(value)
   end
   if #paths == 0 then
-    paths[1] = tostring(cx.active.current.cwd)
+    local h = cx.active.current.hovered.url
+    paths[1] = tostring(h)
+    selected = h:name()
   else
     selected = #paths
   end
@@ -38,14 +40,13 @@ M.entry = function()
   local paths, selected = get_paths()
   local total, err = get_total(paths)
   if err then
-    ya.notify { content = err, level = 'error', timeout = 5, title = 'Size' }
+    ya.notify { content = err, level = 'error', timeout = 3, title = 'Size' }
     return
   end
   ya.notify {
-    content = (selected > 0 and 'Selected ' .. selected .. ' items: ' or 'Current dir: ')
-      .. total,
+    content = selected .. (type(selected) == 'number' and ' selected' or '') .. ': ' .. total,
     -- level = 'info',
-    timeout = 5,
+    timeout = 3,
     title = 'Size',
   }
 end
