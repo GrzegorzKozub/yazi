39ba5665fae561582a78abf795f47047f61bf2a3
greg <grzegorz.kozub@gmail.com>
2025-03-17 08:25:09 +0100

    update git but it does not work

--- init.lua
+++ init.lua
@@ -4,25 +4,25 @@ require('session'):setup { sync_yanked = true }
 
 -- git.yazi
 
-THEME.git = THEME.git or {}
+th.git = th.git or {}
 
-THEME.git.ignored = ui.Style():fg 'darkgray'
-THEME.git.ignored_sign = '!'
+th.git.ignored = ui.Style():fg 'darkgray'
+th.git.ignored_sign = '!'
 
-THEME.git.untracked = ui.Style():fg 'yellow'
-THEME.git.untracked_sign = '*'
+th.git.untracked = ui.Style():fg 'yellow'
+th.git.untracked_sign = '*'
 
-THEME.git.added = ui.Style():fg 'green'
-THEME.git.added_sign = '+'
+th.git.added = ui.Style():fg 'green'
+th.git.added_sign = '+'
 
-THEME.git.modified = ui.Style():fg 'blue'
-THEME.git.modified_sign = '~'
+th.git.modified = ui.Style():fg 'blue'
+th.git.modified_sign = '~'
 
-THEME.git.updated = ui.Style():fg 'blue'
-THEME.git.updated_sign = '~'
+th.git.updated = ui.Style():fg 'blue'
+th.git.updated_sign = '~'
 
-THEME.git.deleted = ui.Style():fg 'red'
-THEME.git.deleted_sign = '-'
+th.git.deleted = ui.Style():fg 'red'
+th.git.deleted_sign = '-'
 
 require('git'):setup()
 
