9dfd9c4dd4a336eae59665b5f5a9644af161ee35
greg <grzegorz.kozub@gmail.com>
2025-03-28 13:17:11 +0100

    compress done

--- plugins/compress.yazi/main.lua
+++ plugins/compress.yazi/main.lua
@@ -2,13 +2,71 @@ local M = {}
 
 local windows = ya.target_family() == 'windows'
 
+local function find_packer(archive_name)
+  local unix = {
+    ['%.7z$'] = { cmd = '7z', args = { 'a' } },
+    ['%.tar$'] = { cmd = 'tar', args = { 'rpf' } },
+    ['%.tar.gz$'] = { cmd = 'tar', args = { 'rpf' }, compressor = 'gzip' },
+    ['%.tar.bz2$'] = { cmd = 'tar', args = { 'rpf' }, compressor = 'bzip2' },
+    ['%.tar.xz$'] = { cmd = 'tar', args = { 'rpf' }, compressor = 'xz' },
+    ['%.tar.zst$'] = {
+      cmd = 'tar',
+      args = { 'rpf' },
+      compressor = 'zstd',
+      compressor_args = { '--rm' },
+    },
+    ['%.zip$'] = { cmd = 'zip', args = { '-r' } },
+  }
+  local win = {
+    ['%.7z$'] = { cmd = '7z', args = { 'a' } },
+    ['%.tar$'] = { cmd = 'tar', args = { 'rpf' } },
+    ['%.tar.bz2$'] = {
+      cmd = 'tar',
+      args = { 'rpf' },
+      compressor = '7z',
+      compressor_args = { 'a', '-tbzip2', '-sdel', archive_name },
+    },
+    ['%.tar.gz$'] = {
+      cmd = 'tar',
+      args = { 'rpf' },
+      compressor = '7z',
+      compressor_args = { 'a', '-tgzip', '-sdel', archive_name },
+    },
+    ['%.tar.xz$'] = {
+      cmd = 'tar',
+      args = { 'rpf' },
+      compressor = '7z',
+      compressor_args = { 'a', '-txz', '-sdel', archive_name },
+    },
+    ['%.tar.zst$'] = {
+      cmd = 'tar',
+      args = { 'rpf' },
+      compressor = 'zstd',
+      compressor_args = { '--rm' },
+    },
+    ['%.zip$'] = { cmd = '7z', args = { 'a', '-tzip' } },
+  }
+  for pattern, packer in pairs(windows and win or unix) do
+    if archive_name:match(pattern) then
+      return packer
+    end
+  end
+  return nil
+end
+
+local function installed(cmd)
+  local unix = 'command -v %s >/dev/null 2>&1'
+  local win = 'where %s > nul 2>&1'
+  return os.execute(string.format(windows and win or unix, cmd))
+end
+
 local get_paths = ya.sync(function()
   local paths, names, items = {}, {}, {}
   for _, value in pairs(cx.active.selected) do
     paths[#paths + 1] = tostring(value:parent())
     names[#names + 1] = tostring(value:name())
   end
-  if #paths == 0 then
+  if #paths == 0 and cx.active.current.hovered then
     local h = cx.active.current.hovered
     paths[1] = tostring(h.url:parent())
     names[1] = tostring(h.name)
@@ -19,7 +77,7 @@ local get_paths = ya.sync(function()
     end
     table.insert(items[paths[index]], value)
   end
-  return items, cx.active.current.cwd
+  return items, tostring(cx.active.current.cwd)
 end)
 
 local function join(dir, name)
@@ -35,13 +93,13 @@ local function exists(path)
   return false
 end
 
-local function get_archive_path(dir, name, cmd)
+local function get_archive_path(dir, name, packer)
   local path = join(dir, name)
   while true do
     if exists(path) then
       return true, path, name
     end
-    if cmd.fmt and not name:match '%.tar$' then
+    if packer.compressor and not name:match '%.tar$' then
       name = name:match '(.*%.tar)'
       path = join(dir, name)
     else
@@ -51,61 +109,60 @@ local function get_archive_path(dir, name, cmd)
   return false, path, name
 end
 
-local function find_command(archive_name)
-  local unix = {
-    ['%.7z$'] = { cmd = '7z', args = { 'a' } },
-    ['%.tar$'] = { cmd = 'tar', args = { 'rpf' } },
-    ['%.tar.gz$'] = { cmd = 'tar', args = { 'rpf' }, fmt = 'gzip' },
-    ['%.tar.bz2$'] = { cmd = 'tar', args = { 'rpf' }, fmt = 'bzip2' },
-    ['%.tar.xz$'] = { cmd = 'tar', args = { 'rpf' }, fmt = 'xz' },
-    ['%.tar.zst$'] = { cmd = 'tar', args = { 'rpf' }, fmt = 'zstd', fmt_args = { '--rm' } },
-    ['%.zip$'] = { cmd = 'zip', args = { '-r' } },
-  }
-  local win = {
-    ['%.7z$'] = { cmd = '7z', args = { 'a' } },
-    ['%.tar$'] = { cmd = 'tar', args = { 'rpf' } },
-    ['%.tar.bz2$'] = {
-      cmd = 'tar',
-      args = { 'rpf' },
-      fmt = '7z',
-      fmt_args = { 'a', '-tbzip2', '-sdel', archive_name },
-    },
-    ['%.tar.gz$'] = {
-      cmd = 'tar',
-      args = { 'rpf' },
-      fmt = '7z',
-      fmt_args = { 'a', '-tgzip', '-sdel', archive_name },
-    },
-    ['%.tar.xz$'] = {
-      cmd = 'tar',
-      args = { 'rpf' },
-      fmt = '7z',
-      fmt_args = { 'a', '-txz', '-sdel', archive_name },
-    },
-    ['%.tar.zst$'] = {
-      cmd = 'tar',
-      args = { 'rpf' },
-      fmt = 'zstd',
-      fmt_args = { '--rm' },
-    },
-    ['%.zip$'] = { cmd = '7z', args = { 'a', '-tzip' } },
-  }
-  for pattern, cmd in pairs(windows and win or unix) do
-    if archive_name:match(pattern) then
-      return cmd
-    end
+local function str(table)
+  local result = ''
+  for i, value in ipairs(table) do
+    result = result .. (i == 1 and '' or ' ') .. value
   end
-  return nil
-end
-
-local function installed(cmd)
-  local unix = 'command -v %s >/dev/null 2>&1'
-  local win = 'where %s > nul 2>&1'
-  return os.execute(string.format(windows and win or unix, cmd))
+  return result
 end
 
 local function error(message)
   ya.notify { content = message, level = 'error', timeout = 3, title = 'Compress' }
+end
+
+local function success(cmd, args, status, err)
+  if not status or not status.success then
+    error(
+      string.format(
+        'Command %s %s ... failed with exit code %s',
+        cmd,
+        str(args),
+        status and status.code or err
+      )
+    )
+    return false
+  end
+  return true
+end
+
+local function pack(items, packer, archive_path)
+  for path, names in pairs(items) do
+    local status, err = Command(packer.cmd)
+      :args(packer.args or {})
+      :arg(archive_path)
+      :args(names)
+      :cwd(path)
+      :spawn()
+      :wait()
+    if not success(packer.cmd, packer.args or {}, status, err) then
+      return false
+    end
+  end
+  return true
+end
+
+local function compress(packer, archive_temp, cwd)
+  local status, err = Command(packer.compressor)
+    :args(packer.compressor_args or {})
+    :arg(archive_temp)
+    :cwd(cwd)
+    :spawn()
+    :wait()
+  if not success(packer.compressor, packer.compressor_args or {}, status, err) then
+    return false
+  end
+  return true
 end
 
 M.entry = function()
@@ -115,62 +172,36 @@ M.entry = function()
   if event ~= 1 then
     return
   end
-  local cmd = find_command(archive_name)
-  if not cmd then
+  local packer = find_packer(archive_name)
+  if not packer then
     error 'Format not supported'
     return
   end
-  if not installed(cmd.cmd) then
-    error('Command ' .. cmd.cmd .. ' not installed')
+  if not installed(packer.cmd) then
+    error('Command ' .. packer.cmd .. ' not installed')
     return
   end
-  if cmd.fmt and not installed(cmd.fmt) then
-    error('Command ' .. cmd.fmt .. ' not installed')
+  if packer.compressor and not installed(packer.compressor) then
+    error('Command ' .. packer.compressor .. ' not installed')
     return
   end
   local items, cwd = get_paths()
-  local archive_exists, archive_path
-  archive_exists, archive_path, archive_name = get_archive_path(cwd, archive_name, cmd)
-  if archive_exists then
-    error('File ' .. archive_name .. ' already exists')
+  if next(items) == nil then
     return
   end
-
-  -- Add to output archive in each path, their respective files
-  -- for path, names in pairs(path_fnames) do
-  -- 	local archive_status, archive_err =
-  -- 		Command(archive_cmd):args(archive_args):arg(output_url):args(names):cwd(path):spawn():wait()
-  -- 	if not archive_status or not archive_status.success then
-  -- 		notify_error(
-  -- 			string.format(
-  -- 				"%s with selected files failed, exit code %s",
-  -- 				archive_args,
-  -- 				archive_status and archive_status.code or archive_err
-  -- 			),
-  -- 			"error"
-  -- 		)
-  -- 	end
-  -- end
-
-  -- Use compress command if needed
-  -- if archive_compress then
-  -- 	local compress_status, compress_err =
-  -- 		Command(archive_compress):args(archive_compress_args):arg(output_name):cwd(output_dir):spawn():wait()
-  -- 	if not compress_status or not compress_status.success then
-  -- 		notify_error(
-  -- 			string.format(
-  -- 				"%s with %s failed, exit code %s",
-  -- 				archive_compress,
-  -- 				output_name,
-  -- 				compress_status and compress_status.code or compress_err
-  -- 			),
-  -- 			"error"
-  -- 		)
-  -- 	end
-  -- end
-
+  local archive_exists, archive_path, archive_temp = get_archive_path(cwd, archive_name, packer)
+  if archive_exists then
+    error('File ' .. archive_temp .. ' already exists')
+    return
+  end
+  if not pack(items, packer, archive_path) then
+    return
+  end
+  if packer.compressor and not compress(packer, archive_temp, cwd) then
+    return
+  end
   ya.notify {
-    content = 'cmprs ' .. archive_path,
+    content = 'Created archive ' .. archive_name,
     level = 'info',
     timeout = 3,
     title = 'Compress',
