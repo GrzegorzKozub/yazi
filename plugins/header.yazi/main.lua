d505ae62e75fba163fae75b82e8b18248db615b1
greg <grzegorz.kozub@gmail.com>
2025-12-24 08:26:41 +0100

    header just like status

--- plugins/header.yazi/main.lua
+++ plugins/header.yazi/main.lua
@@ -1,64 +1,71 @@
 local M = {}
 
-local function append(count, style, val)
-  count[#count + 1] = ui.Span(''):style(style):reverse()
-  count[#count + 1] = ui.Span(val):style(style)
-  count[#count + 1] = ui.Span(''):style(style):reverse()
-end
-
-local function append2(count, style, val)
-  count[#count + 1] = ui.Span(''):style(style)
-  count[#count + 1] = ui.Span(val):style(style)
-  count[#count + 1] = ui.Span(''):style(style)
-end
-
-function Header:cwd()
-  local cwd1 = { ui.Span(tostring(self._current.cwd)):style(th.mgr.cwd) }
-
-  local cwd = self._current.cwd
-  if cwd.is_search then
-    append(cwd1, th.mgr.find_position, string.format('search: %s', cwd.domain))
-  end
-  -- return ui.Line {
-  --   ui.Span ' ',
-  --   ui.Span(self:flags()):style(th.mgr.cwd),
-  -- }
-  return ui.Line(cwd1)
-end
-
-function Header:flags()
-  local cwd = self._current.cwd
-  local filter = self._current.files.filter
-  local finder = self._tab.finder
-  local t = {}
-  if cwd.is_search then
-    t[#t + 1] = string.format('search: %s', cwd.domain)
-  end
-  if filter then
-    t[#t + 1] = string.format('filter: %s', filter)
-  end
-  if finder then
-    t[#t + 1] = string.format('find: %s', finder)
-  end
-  return #t == 0 and '--' or ' (' .. table.concat(t, ', ') .. ')'
-end
-
-function Header:count()
-  local s, y, count = #self._tab.selected, #cx.yanked, {}
-  if s > 0 then
-    append(count, th.mgr.count_selected, string.format('%d', s))
-  end
-  if y > 0 then
-    count[#count + 1] = ui.Span ' '
-    append(
-      count,
-      cx.yanked.is_cut and th.mgr.count_cut or th.mgr.count_copied,
-      string.format('%d', y)
+local function cwd()
+  return ui.Line {
+    ui.Span(
+      ui.truncate(ya.readable_path(tostring(cx.active.current.cwd)), { max = 64, rtl = true })
     )
-  end
-  return ui.Line(count)
+      :style(th.mgr.cwd),
+  }
 end
 
-M.setup = function() end
+local function count(style, val)
+  return ui.Line {
+    ui.Span(''):style(style):reverse(),
+    ui.Span(val):style(style),
+    ui.Span(''):style(style):reverse(),
+  }
+end
+
+local function selected()
+  local s = #cx.active.selected
+  return s > 0 and ui.Line(count(th.mgr.count_selected, string.format('%d', s))) or ''
+end
+
+local function yanked()
+  local y = #cx.yanked
+  return y > 0
+      and ui.Line(
+        count(
+          cx.yanked.is_cut and th.mgr.count_cut or th.mgr.count_copied,
+          string.format('%d', y)
+        )
+      )
+    or ''
+end
+
+-- function Header:flags()
+--   local cwd = self._current.cwd
+--   local filter = self._current.files.filter
+--   local finder = self._tab.finder
+--   local t = {}
+--   if cwd.is_search then
+--     t[#t + 1] = string.format('search: %s', cwd.domain)
+--   end
+--   if filter then
+--     t[#t + 1] = string.format('filter: %s', filter)
+--   end
+--   if finder then
+--     t[#t + 1] = string.format('find: %s', finder)
+--   end
+--   return #t == 0 and '--' or ' (' .. table.concat(t, ', ') .. ')'
+-- end
+
+local function space()
+  return ui.Span ' '
+end
+
+M.setup = function()
+  Header:children_remove(1, Header.LEFT)
+  local left = { cwd }
+  for i, value in ipairs(left) do
+    Header:children_add(value, i, Header.LEFT)
+  end
+  Header:children_remove(1, Header.RIGHT)
+  local right = { selected, space, yanked }
+  for i, value in ipairs(right) do
+    Header:children_add(value, i, Header.RIGHT)
+  end
+end
 
 return M
